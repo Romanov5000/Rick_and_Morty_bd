2f909ea8a4c7ecfe30f7c4cb7063cb773069e572
Romanov5000 <romanovvladislav97@gmail.com>
2020-09-24 22:03:35 +0300

    Updates

---
+++ precache-manifest.50db60cb00fd48089e6584aa8872302a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0606760a8277bf7974e36b775e87ea9",
+    "url": "/Romanov5000/Rick_and_Morty_bd/index.html"
+  },
+  {
+    "revision": "0ea894ed0b801467bd09",
+    "url": "/Romanov5000/Rick_and_Morty_bd/static/css/main.3d14f2bd.chunk.css"
+  },
+  {
+    "revision": "6093aa1006102682b694",
+    "url": "/Romanov5000/Rick_and_Morty_bd/static/js/2.8cc6b26c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Romanov5000/Rick_and_Morty_bd/static/js/2.8cc6b26c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ea894ed0b801467bd09",
+    "url": "/Romanov5000/Rick_and_Morty_bd/static/js/main.dd5373d3.chunk.js"
+  },
+  {
+    "revision": "2d0b365ed7ec8c1f6835",
+    "url": "/Romanov5000/Rick_and_Morty_bd/static/js/runtime-main.aeaa04c0.js"
+  }
+]);
